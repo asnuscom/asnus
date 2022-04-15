c4e796dbfee136d9e676c19e6b56c99e54ed890f
İhsan SUNMAN <ihsansunman@asnus.com>
2022-04-15 13:22:09 +0300

    Fix Netlify Error

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,13 +16,6 @@ export default class Body extends React.Component {
 
           {/* Google Analytics */}
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-X775Z5FTRJ"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date());
-
-            gtag('config', 'G-X775Z5FTRJ');
-          </script>
 
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initialScale=1.0" />
